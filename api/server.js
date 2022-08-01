08dcc8d339fccd2e90b9db1a21c14a9fe29c6d49
nmemarcoding <nmemarcoding@gmail.com>
2022-08-01 11:12:27 -0700

    mongo db conected to server

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 const app = express();
+
 var corsOptions = {
     origin: "http://localhost:8081"
 };
@@ -17,12 +18,54 @@ app.use(
         httpOnly: true
     })
 );
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
-});
-// set port, listen for requests
+
+const db = require("./models");
+const Role = db.role;
+db.mongoose
+    .connect(`mongodb+srv://nmemarcoding:Nima1377@cluster0.hjtd5.mongodb.net/?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'user' to roles collection");
+            });
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'moderator' to roles collection");
+            });
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
-})
+});
